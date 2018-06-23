8c8ca1cea3a0d93741102e5683647900bef91825
Guilherme Alves <guiigos.alves@gmail.com>
2018-06-23 11:31:59 -0300

    Correction update

--- install/node/global.sh
+++ install/node/global.sh
@@ -11,7 +11,11 @@ function installnpm() {
     msgcheck $1;
     if $3 == true; then
       msgupdate $1;
-      npm update -g $2;
+      if $4 == true; then
+        sudo npm update -g $2;
+      else
+        npm update -g $2;
+      fi
       msgupdated $1;
     fi
   else
@@ -35,26 +39,26 @@ else
   msginstalled "node";
 fi
 
-installnpm "yo" yo true;                                # https://www.npmjs.com/package/yo
-installnpm "yo" generator-stack-node true;              # https://www.npmjs.com/package/generator-stack-node
-installnpm "gulp" gulp true;                            # https://www.npmjs.com/package/gulp
-installnpm "cordova" cordova true;                      # https://www.npmjs.com/package/cordova
-installnpm "ionic" ionic true;                          # https://www.npmjs.com/package/ionic
-installnpm "ngrok" ngrok true;                          # https://www.npmjs.com/package/ngrok
-installnpm "nodemon" nodemon true;                      # https://www.npmjs.com/package/nodemon
-installnpm "http-server" http-server true;              # https://www.npmjs.com/package/http-server
-installnpm "pm2" pm2 true;                              # https://www.npmjs.com/package/pm2
-installnpm "gitignore" gitignore true;                  # https://www.npmjs.com/package/gitignore
-installnpm "figlet" figlet-cli true;                    # https://www.npmjs.com/package/figlet-cli
-installnpm "mddir" mddir true;                          # https://www.npmjs.com/package/mddir
-installnpm "sort-package-json" sort-package-json true;  # https://www.npmjs.com/package/sort-package-json
-installnpm "notes" notes true;                          # https://www.npmjs.com/package/notes
-installnpm "npm-dview" npm-dview true;                  # https://www.npmjs.com/package/npm-dview
-installnpm "now" now true;                              # https://www.npmjs.com/package/now
-installnpm "ng" @angular/cli true;                      # https://www.npmjs.com/package/@angular/cli
-installnpm "bower" bower true true;                     # https://www.npmjs.com/package/bower
-installnpm "ios-sim" ios-sim true true;                 # https://www.npmjs.com/package/ios-sim
-installnpm "ios-deploy" ios-deploy true true;           # https://www.npmjs.com/package/ios-deploy
+installnpm "yo" yo true false;                                # https://www.npmjs.com/package/yo
+installnpm "yo" generator-stack-node true false;              # https://www.npmjs.com/package/generator-stack-node
+installnpm "gulp" gulp true false;                            # https://www.npmjs.com/package/gulp
+installnpm "cordova" cordova true false;                      # https://www.npmjs.com/package/cordova
+installnpm "ionic" ionic true false;                          # https://www.npmjs.com/package/ionic
+installnpm "ngrok" ngrok true false;                          # https://www.npmjs.com/package/ngrok
+installnpm "http-server" http-server true false;              # https://www.npmjs.com/package/http-server
+installnpm "gitignore" gitignore true false;                  # https://www.npmjs.com/package/gitignore
+installnpm "figlet" figlet-cli true false;                    # https://www.npmjs.com/package/figlet-cli
+installnpm "mddir" mddir true false;                          # https://www.npmjs.com/package/mddir
+installnpm "sort-package-json" sort-package-json true false;  # https://www.npmjs.com/package/sort-package-json
+installnpm "notes" notes true false;                          # https://www.npmjs.com/package/notes
+installnpm "npm-dview" npm-dview true false;                  # https://www.npmjs.com/package/npm-dview
+installnpm "now" now true false;                              # https://www.npmjs.com/package/now
+installnpm "ng" @angular/cli true false;                      # https://www.npmjs.com/package/@angular/cli
+installnpm "bower" bower true true;                           # https://www.npmjs.com/package/bower
+installnpm "nodemon" nodemon true true;                       # https://www.npmjs.com/package/nodemon
+installnpm "pm2" pm2 true true;                               # https://www.npmjs.com/package/pm2
+installnpm "ios-sim" ios-sim true true;                       # https://www.npmjs.com/package/ios-sim
+installnpm "ios-deploy" ios-deploy true true;                 # https://www.npmjs.com/package/ios-deploy
 
 # Clean
 msg "Cache clean NPM";
