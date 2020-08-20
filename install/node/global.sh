5b8b9ad619a0ce331b1b5637cfd9d19387eb197e
Guilherme Alves <guiigos.alves@gmail.com>
2020-08-20 16:02:40 -0300

    Change global packages

--- install/node/global.sh
+++ install/node/global.sh
@@ -48,24 +48,25 @@ npm config set init.author.email "guiigos.alves@gmail.com";
 npm config set save-prefix="~";
 
 # Globals
+installnpm "yo" yo true false;                                # https://www.npmjs.com/package/yo
 installnpm "typescript" typescript true false;                # https://www.npmjs.com/package/typescript
+installnpm "create-next-app" create-next-app true false;      # https://www.npmjs.com/package/create-next-app
 installnpm "create-react-app" create-react-app true false;    # https://www.npmjs.com/package/create-react-app
 installnpm "react-native-cli" react-native-cli true false;    # https://www.npmjs.com/package/react-native-cli
-installnpm "yo" yo true false;                                # https://www.npmjs.com/package/yo
-installnpm "figlet" figlet-cli true false;                    # https://www.npmjs.com/package/figlet-cli
 installnpm "ngrok" ngrok true false;                          # https://www.npmjs.com/package/ngrok
-installnpm "sort-package-json" sort-package-json true false;  # https://www.npmjs.com/package/sort-package-json
+installnpm "nodemon" nodemon true true;                       # https://www.npmjs.com/package/nodemon
 installnpm "npm-dview" npm-dview true false;                  # https://www.npmjs.com/package/npm-dview
 installnpm "npm-check" npm-check true false;                  # https://www.npmjs.com/package/npm-check
 installnpm "http-server" http-server true false;              # https://www.npmjs.com/package/http-server
 installnpm "json-server" json-server true false;              # https://www.npmjs.com/package/json-server
-installnpm "workin-hard" workin-hard true false;              # https://www.npmjs.com/package/workin-hard
-installnpm "notes" notes true false;                          # https://www.npmjs.com/package/notes
 installnpm "mddir" mddir true false;                          # https://www.npmjs.com/package/mddir
+installnpm "notes" notes true false;                          # https://www.npmjs.com/package/notes
+installnpm "figlet" figlet-cli true false;                    # https://www.npmjs.com/package/figlet-cli
 installnpm "cowsay" cowsay true false;                        # https://www.npmjs.com/package/cowsay
 installnpm "qrip" qrip true false;                            # https://www.npmjs.com/package/qrip
+installnpm "workin-hard" workin-hard true false;              # https://www.npmjs.com/package/workin-hard
+installnpm "sort-package-json" sort-package-json true false;  # https://www.npmjs.com/package/sort-package-json
 installnpm "pm2" pm2 true true;                               # https://www.npmjs.com/package/pm2
-installnpm "nodemon" nodemon true true;                       # https://www.npmjs.com/package/nodemon
 
 # Clean
 msg "Cache clean NPM";
